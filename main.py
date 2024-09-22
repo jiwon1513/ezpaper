b2cc2b802f4401b59e5507387e8cec833747cabe
jiwon1513 <mgo1513@naver.com>
2024-09-22 15:56:07 +0900

    base

--- main.py
+++ main.py
@@ -3,10 +3,8 @@ from googletrans import Translator
 
 translator = Translator()
 
-text='나는 한국인이다.'
-cn_result= translator.translate(text, dest='zh-cn')
+text='Text-to-image models offer unprecedented freedom to guide creation through natural lan guage. Yet, it is unclear how such freedom can be exercised to generate images of specific unique concepts, modify their appearance, or compose them in new roles and novel scenes. In other words, we ask: how can we use language-guided models to turn our cat into a painting, or imagine a new product based on our favorite toy? Here we present a simple approach that allows such creative freedom. Using only 3-5 images of a user-provided concept, like an object or a style, we learn to represent it through new “words” in the embedding space of a frozen text-to-image model. These “words” can be composed into natural language sentences, guiding personalized creation in an intuitive way. Notably, we f ind evidence that a single word embedding is sufficient for capturing unique and varied concepts. We compare our approach to a wide range of baselines, and demonstrate that it can more faithfully portray the concepts across a range of applications and tasks.'
+result= translator.translate(text, dest='ko')
 
-#번역한 중국어를 다시 한국어로 바꿔보기
-ko_result = translator.translate(cn_result.text, dest='ko')
-print(cn_result.text) #我是韩国人。
-print(ko_result.text) #나는 한국인이다.6
+
+print(result.text) 
